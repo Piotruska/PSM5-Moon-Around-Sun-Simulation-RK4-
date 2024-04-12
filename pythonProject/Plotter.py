3fa9ade009ca1edf9f3e3f50bec6b48fccb4c794
s27051 <s27051@pjwstk.edu.pl>
2024-04-12 15:21:34 +0200

    Added animate_orbits method to Plotter.py

--- pythonProject/Plotter.py
+++ pythonProject/Plotter.py
@@ -18,3 +18,33 @@ def plot_orbits(xs, ys, xm, ym, scale_factor=2):
     plt.axis('equal')
     plt.show()
 
+
+def animate_orbits(xs, ys, xm, ym, N, scale_factor=2):
+    fig, ax = plt.subplots(figsize=(10, 6))
+    ax.set_facecolor('black')
+    ax.plot(xs, ys, 'b-', label="Earth's Orbit", linewidth=0.5)  # Earth's orbit
+    ax.plot([x + scale_factor * (xm[i] - x) for i, x in enumerate(xs)],
+            [y + scale_factor * (ym[i] - y) for i, y in enumerate(ys)],
+            'w--', label="Moon's Exaggerated Orbit", linewidth=0.5)
+
+    ax.plot(0, 0, 'yo', markersize=20)  # Sun
+    earth, = ax.plot([], [], 'o', color='blue', markersize=10)
+    moon, = ax.plot([], [], 'o', color='gray', markersize=5)
+
+    def init():
+        earth.set_data([], [])
+        moon.set_data([], [])
+        return earth, moon,
+
+    def update(frame):
+        ex, ey = xs[frame], ys[frame]
+        mx, my = xs[frame] + scale_factor * (xm[frame] - xs[frame]), ys[frame] + scale_factor * (ym[frame] - ys[frame])
+
+        earth.set_data(ex, ey)
+        moon.set_data(mx, my)
+        return earth, moon,
+
+    ani = FuncAnimation(fig, update, frames=N, init_func=init, blit=True, repeat=True, interval=5)
+    plt.legend()
+    plt.axis('equal')
+    plt.show()
